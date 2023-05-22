04201a47909a15553893fe247e414bee8e1ca58e
SilvesterSpath <spath.sz@gmail.com>
2023-05-22 06:11:51 +0200

    type guard

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,27 @@ class Sorter {
   } */
 
   //totally equivalent this line
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+        // All of this only works if collection is a number[]
         // If collection is an array of numbers
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
         }
-
+        // Only going to work if collection is a string
         // If collection is a string, do this logic instead:
         // ~~~logic to compare and swap characters in a string
+        if (this.collection instanceof String) {
+        }
       }
     }
   }
