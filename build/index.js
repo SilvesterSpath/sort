7593b11a2f1f09bead31bd5be34a455c2c146b88
SilvesterSpath <spath.sz@gmail.com>
2023-05-22 05:52:33 +0200

    string vs number[]

--- build/index.js
+++ build/index.js
@@ -13,12 +13,14 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
+                // If collection is an array of numbers
                 if (this.collection[j] > this.collection[j + 1]) {
-                    let temp;
-                    temp = this.collection[j];
+                    const temp = this.collection[j];
                     this.collection[j] = this.collection[j + 1];
                     this.collection[j + 1] = temp;
                 }
+                // If collection is a string, do this logic instead:
+                // ~~~logic to compare and swap characters in a string
             }
         }
     }
